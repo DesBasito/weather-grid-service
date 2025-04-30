a8388f7ac428c8e238757fc518fee31ecb175008
DesBasito <116246777+DesBasito@users.noreply.github.com>
2025-04-30 22:13:18 +0600

    Добавил Maven Wrapper и начальную настройку приложений Spring Boot
    Введен Maven Wrapper с необходимыми файлами конфигурации для упрощения использования Maven и управления версиями. Кроме того, была создана начальная структура приложений Spring Boot, включая основные настройки класса и Gitignore, чтобы исключить ненужные файлы.

---
+++ src/main/java/kg/manurov/weathergridservice/WeatherGridServiceApplication.java
@@ -0,0 +1,13 @@
+package kg.manurov.weathergridservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherGridServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherGridServiceApplication.class, args);
+	}
+
+}
