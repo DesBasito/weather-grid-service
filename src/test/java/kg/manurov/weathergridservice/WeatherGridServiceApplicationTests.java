913de8aba04a554477612fcf4226f133e6c1384a
DesBasito <116246777+DesBasito@users.noreply.github.com>
2025-04-30 22:18:08 +0600

    Добавил начальную настройку проекта с помощью зависимостей для загрузки Spring Этот коммит представляет первоначальную конфигурацию проекта. Он включает в себя файл pom.xml с необходимыми зависимостями.

---
+++ src/test/java/kg/manurov/weathergridservice/WeatherGridServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.manurov.weathergridservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherGridServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
