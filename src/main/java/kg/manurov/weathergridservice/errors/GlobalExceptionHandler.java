2d2719ee70e902abe4a8c64f3998084a917b0726
DesBasito <116246777+DesBasito@users.noreply.github.com>
2025-05-07 12:58:30 +0600

    Добавил глобальный обработчик исключений для централизованной обработки ошибок
    Этот коммит представляет класс `globalexceptionHandler` для управления исключениями приложений в центре. Он обрабатывает различные исключения, такие как «noshelementexception», `nullpointerException` и ошибки проверки, возвращая соответствующие ответы HTTP и ошибки журнала. Это улучшает обслуживание и улучшает пользовательский опыт со стандартизированными сообщениями об ошибках.

---
+++ src/main/java/kg/manurov/weathergridservice/errors/GlobalExceptionHandler.java
@@ -0,0 +1,51 @@
+package kg.manurov.weathergridservice.errors;
+
+import kg.manurov.weathergridservice.services.interfaces.ErrorService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @Autowired
+    private ErrorService errorService;
+    @ExceptionHandler(NoSuchElementException.class)
+    public ErrorResponse noSuchElementException(NoSuchElementException e){
+        log.error(e.getMessage());
+        return ErrorResponse.builder(e, HttpStatus.NOT_FOUND,e.getMessage()).build();
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<ErrorResponseBody> nullPointerExceptions(NullPointerException e){
+        log.error(e.getMessage());
+        return new ResponseEntity<>(errorService.makeResponse(e), HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseBody> validationHandler(MethodArgumentNotValidException ex){
+        log.error(ex.getMessage());
+        return new ResponseEntity<>(errorService.makeResponse(ex.getBindingResult()),HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponseBody> throwIllegalException(IllegalArgumentException ex){
+        log.error(ex.getMessage());
+        return new ResponseEntity<>(errorService.makeResponse(ex), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponseBody> httpMethodException(HttpMessageNotReadableException ex){
+        log.error(ex.getMessage());
+        return new ResponseEntity<>(errorService.makeResponse(ex), HttpStatus.CONFLICT);
+    }
+}
+
